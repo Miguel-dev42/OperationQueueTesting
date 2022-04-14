b3e02908e5405fd9823982e4630079fbcb45681f
Miguel-dev42 <79338375+Miguel-dev42@users.noreply.github.com>
2022-04-14 17:06:54 -0600

    Initial Commit

---
+++ OperationQueueTests/OperationQueueTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OperationQueueTestsApp.swift
+//  OperationQueueTests
+//
+//  Created by Miguel Figueroa on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct OperationQueueTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
